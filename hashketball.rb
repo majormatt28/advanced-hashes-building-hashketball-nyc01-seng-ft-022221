65393a7c72b35e896c9a06443e57d9e5f3572851
Mathew E Newton <elijah2895@hotmail.com>
2021-02-17 01:28:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -78,37 +78,37 @@ def game_hash
           :slam_dunks => 2
         },
         {
-          :player_name => "Alan Anderson",
+          :player_name => "Bismack Biyombo",
           :number => 0,
           :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 22,
+          :blocks => 15,
+          :slam_dunks => 10
         },
         {
-          :player_name => "Alan Anderson",
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
+          :player_name => "DeSagna Diop",
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
           :rebounds => 12,
           :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
         },
         {
-          :player_name => "Alan Anderson",
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
+          :player_name => "Ben Gordon",
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
           :blocks => 1,
-          :slam_dunks => 1
+          :slam_dunks => 0
         },
         {
           :player_name => "Alan Anderson",
