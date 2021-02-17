ae20d91d293476b7d2a6232824a9d7bda950f365
Mathew E Newton <elijah2895@hotmail.com>
2021-02-17 01:24:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -68,14 +68,14 @@ def game_hash
       :players => [
         :player_name => {
           :player_name => "Jeff Adrein",
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         },
         {
           :player_name => "Alan Anderson",
