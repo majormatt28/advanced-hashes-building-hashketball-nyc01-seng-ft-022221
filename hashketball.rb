d72a9a518e150719a6cf6bac776ed1b38f572883
Mathew E Newton <elijah2895@hotmail.com>
2021-02-17 00:53:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,19 @@ def game_hash
           :steals => 3
           :blocks => 1
           :slam_dunks => 1
-        }]
+        },
+        "Reggie Evans" => {
+          :number => 30
+          :shoe => 14
+          :points => 12
+          :rebounds => 12
+          :assits => 12
+          :steals => 12
+          :blocks => 12
+          :slam_dunks => 7
+        },
+        ""
+      ]
     }
   }
 end
