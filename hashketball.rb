d72374e055e8a72e6c951f69a420cc9d76ca5698
Mathew E Newton <elijah2895@hotmail.com>
2021-02-17 01:18:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,8 @@ def game_hash
     :home => {
       :team_name => "",
       :colors => [""],
-      :players => [{
+      :players => [
+        {
         :player_name => "Alan Anderson",
         :number => 0,
         :shoe => 16,
@@ -14,7 +15,52 @@ def game_hash
         :steals => 3,
         :blocks => 1,
         :slam_dunks => 1
-        }]
+      },
+      {
+        :player_name => "Reggie Evans",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
+    }, 
+    {
+      :player_name => "Brook Lopez",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
+    },
+    {
+      :player_name => "Mason Plumlee",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
+    },
+    {
+      :player_name => "Jason Terry",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
+    }
+      ]
     },
     :away => {
       :team_name => "",
