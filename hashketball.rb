70790de2a83a65c8288488ace6ad23d2bf000fa0
Mathew E Newton <elijah2895@hotmail.com>
2021-02-17 00:35:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,10 @@
 # Write your code here!
 def game_hash
   {
-    :home => {:team_name},{:colors},{:players}
+    :home => 
+    {:team_name => " "
+      }, {:colors => [" "]
+        }, {:players => {[]}
+        }
   }
 end
