43b08db0484a563887738db4a7676dbdaf6bd128
Mathew E Newton <elijah2895@hotmail.com>
2021-02-17 01:19:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,14 +18,14 @@ def game_hash
       },
       {
         :player_name => "Reggie Evans",
-      :number => 0,
-      :shoe => 16,
-      :points => 22,
+      :number => 30,
+      :shoe => 14,
+      :points => 12,
       :rebounds => 12,
       :assists => 12,
-      :steals => 3,
-      :blocks => 1,
-      :slam_dunks => 1
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks => 7
     },
     {
       :player_name => "Brook Lopez",
